d4c64650fa69a1c33258340fdfe5bebc2465c9be
Epilow <benjaminchaumet2@gmail.com>
2025-05-12 13:52:21 +0200

    modified:   "modification de l'etape 1.py" 	deleted:    "suprimer fichier qui ne sert plus"

--- Étape 1/etape 1.py
+++ Étape 1/etape 1.py
@@ -7,13 +7,13 @@ print(f"{conf.version} est la version de Scapy.")
 print(f"{conf.iface} est l'interface réseau utilisée.")
 
 #afficher le nom de l'interface réseau utilisée
-print(f"{conf.iface.name} est le nom de l'interface réseau utilisée.")
+#print(f"{conf.iface.name} est le nom de l'interface réseau utilisée.")
 
 #afficher la table de routage
-print(f"La table de route de {conf.iface.name} est: \n {conf.route}")
+print(f"La table de route de {conf.iface} est: \n {conf.route}")
 
 #afficher la passerelle par défaut
-print(f"La passerelle par défaut de {conf.iface.name} est: {conf.route.route('0.0.0.0')[2]}")
+print(f"La passerelle par défaut de {conf.iface} est: {conf.route.route('0.0.0.0')[2]}")
 
 #créer un paquet IP avec UDP et afficher le résumé et le contenu
 packet1 = IP()/UDP()
@@ -78,3 +78,48 @@ RT={'Siami' : {'fonction' : 'chef de departement', 'année de recutement' :2002,
 #afficher le nom et role de Mr Bascou
 print(f'Le nom de la fonction de Mr Bascou est {RT["Bascou"]["fonction"]}')
 
+print('La premiere trame est la suivante :')
+trames[0]
+
+trames[2][IP]
+
+trames[2][IP].dst
+
+trames[2][Raw].load
+
+trames[2][Raw].load.split(sep=None)
+
+trames[2][Raw].load.split(sep=None)[2]
+
+trames[2][Raw].load.split(sep=None)[2].decode('UTF8')
+
+ping=IP(dst='8.8.8.8')/ICMP()/"On peut ajouter ici des données à émettre avec le ping"
+send(ping)
+
+#envoyer un ping avec plusieur arguments
+#send(ping, loop=1)
+
+#envoyer un ping avec une boucle infinie
+#send(ping, loop=1, inter=1)
+
+trameping=Ether()/IP(dst='10.2.12.4')/ICMP()/"On peut ajouter ici des données à émettre avec le ping"
+sendp(trameping, iface="wlp19s0")
+
+ping=IP(dst='10.2.12.4')/ICMP()/"On peut ajouter ici des données à émettre avec le ping"
+sr(ping)
+
+ping=IP(dst='10.2.12.4')/ICMP()/"On peut ajouter ici des données à émettre avec le ping"
+ok, nonok = srloop(ping, count=10, inter=1)
+
+ICMP_types = { 0 : 'Echo-Reply', 3 : 'Destination Unreachable', 8 : 'Echo'}
+def print_icmp (packet) :
+    type = packet[ICMP].type
+    ips = packet[IP].src
+    ipd = packet[IP].dst
+    if ips==iface_ip :
+        print(f"Emission d'un paquet ICMP {ICMP_types[type]} vers {ipd}")
+    else :
+        print(f"Réception d'un paquet ICMP {ICMP_types[type]} en provenance de {ips}")
+
+iface_ip=get_if_addr(conf.iface)
+sniff(filter="icmp", prn=print_icmp, store=0, iface='wlp19s0', count=4)
